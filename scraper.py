d0917450351d4c8f5c852106d15d672929e3e7f1
Alex-Mulligan <mulligaa@uci.edu>
2020-02-08 17:14:56 -0800

    Better link checking in scraper

--- scraper.py
+++ scraper.py
@@ -3,19 +3,19 @@ from urllib.parse import urlparse, urldefrag
 from bs4 import BeautifulSoup
 
 def scraper(url, resp):
-    if resp.status in range(200,400):
+    if resp.status in range(200,300):
         links, tokens = extract_next_links(url, resp)
         return ([link for link in links if is_valid(link)], tokens)
-    elif resp.status in range(400,600):
+    elif resp.status in range(300,600):
         print(f"{url} gave status {resp.status}")
-        return (list(), list())
+        return (list(), "")
     elif resp.status in range(600,607):
         with open("600errors.txt", "a") as file:
             file.write(f"{url} gave status {resp.status} with message:\n{resp.error}\n")
         print(f"{url} gave status {resp.status} with message: {resp.error}")
-        return (list(),list())
+        return (list(), "")
     else:
-        return (list(), list())
+        return (list(), "")
 
 def extract_next_links(url, resp):
     # Implementation requred...
@@ -29,28 +29,42 @@ def extract_next_links(url, resp):
     return (links, tokens)
 
 def extract_text(soup):
-    blacklist = ['[document]','noscript','header','html','meta','head','input','script']
+    blacklist = ['[document]','noscript','header','html','meta','head','input','script', 'footer', 'div', 'style', 'i']
     page_text = ''
-    page_list = []
+#     page_list = []
     text = soup.findAll(text=True)
     for t in text:
         if t.parent.name not in blacklist:
             page_text += "{}".format(t) #creates a long string representing the page
-            page_list.append(t) #adds each 'line' to a list
+#             page_list.append(t) #adds each 'line' to a list
     tokens = []
     regex = re.compile(r"[A-Za-z0-9]+")
-    for line in page_list:
-        tokens.extend(regex.findall(line.lower()))
-    return tokens #or page_text for a large string representation of the page
+#     for line in page_list:
+#         tokens.extend(regex.findall(line.lower()))
+    
+    return page_text #or page_text for a large string representation of the page
 
 def is_valid(url):
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        if len(url) > 200:
+        if len(url) > 250:
             return False
         if not check_robots_txt(parsed):
+            return False
+        if not other_link_checking(parsed):
+            return False
+        if re.match(
+            r".*\.(css|js|bmp|gif|jpe?g|ico"
+            + r"|png|tiff?|mid|mp2|mp3|mp4"
+            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+            + r"|epub|dll|cnf|tgz|sha1"
+            + r"|thmx|mso|arff|rtf|jar|csv"
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz"
+            + r"|ppsx|ps|z|mat|m|odc)$", parsed.query.lower()):
             return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
@@ -60,7 +74,8 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz"
+            + r"|ppsx|ps|z|mat|m|odc)$", parsed.path.lower())
 
     except TypeError:
         print ("TypeError for ", parsed)
@@ -68,21 +83,21 @@ def is_valid(url):
 
 def check_robots_txt(parsed):
     try:
-        if re.match(r".*ics\.uci\.edu", parsed.netloc):
+        if re.match(r"ics\.uci\.edu", parsed.netloc) or re.match(r".*\.ics.uci.edu",parsed.netloc):
             if re.match(r"^/bin/?", parsed.path) or re.match(r"^/~mpufal/?", parsed.path):
                 return False
             return True
-        elif re.match(r".*cs\.uci\.edu", parsed.netloc):
+        elif re.match(r"cs\.uci\.edu", parsed.netloc) or  re.match(r".*\.cs\.uci\.edu", parsed.netloc):
             if re.match(r"/wp-admin/?", parsed.path):
                 if not re.match(r"/wp-admin/admin-ajax.php", parsed.path):
                     return False
             return True
-        elif re.match(r".*stat\.uci\.edu", parsed.netloc):
+        elif re.match(r"stat\.uci\.edu", parsed.netloc) or re.match(r".*\.stat\.uci\.edu", parsed.netloc):
             if re.match(r"/wp-admin/?", parsed.path):
                 if not re.match(r"/wp-admin/admin-ajax.php"):
                     return False
             return True
-        elif re.match(r".*informatics\.uci\.edu", parsed.netloc):
+        elif re.match(r"informatics\.uci\.edu", parsed.netloc) or re.match(r".*\.informatics\.uci\.edu", parsed.netloc):
             if re.match(r"/wp-admin/?", parsed.path):
                 if not re.match(r"/wp-admin/admin-ajax.php", parsed.path):
                     return False
@@ -98,16 +113,16 @@ def check_robots_txt(parsed):
                     return False
             return True
         elif re.match(r"^(www\.)?today\.uci\.edu", parsed.netloc) and re.match(r"^department/information_computer_sciences/.*", parsed.path):
-            if re.match(r".*/calendar/.*\?.*types.*", parsed.path):
+            if re.match(r".*/calendar/.*\?.*types.*", parsed.path+'?'+parsed.query):
                 return False
-            elif re.match(r".*/browse.*\?.*types.*", parsed.path):
+            elif re.match(r".*/browse.*\?.*types.*", parsed.path+'?'+parsed.query):
                 return False
             elif re.match(r".*/calendar/week/?", parsed.path):
                 return False
             elif re.match(r".*/calendar/20[0-2]\d.*", parsed.path):
                 return False
             elif re.match(r".*/search/?", parsed.path):
-                if not (re.match(r".*/search/events\.ics/?", parsed.path) or re.match(r".*/search/events.xml/?", parsed.path)):
+                if not re.match(r".*/search/events\.(ics|xml)/?", parsed.path):
                     return False
             return True
         else:
@@ -115,5 +130,16 @@ def check_robots_txt(parsed):
     except:
         print(f"Error for {parsed}")
         raise
+    
+def other_link_checking(parsed):
+    if re.match(r".*/(pdf|e?pub)/", parsed.path.lower()): #maybe add e?pub to the re
+        return False
+    if re.match(r".*/.*-(jpe?g|png)", parsed.path.lower()):
+        return False
+    if re.match(r".*-(jpe?g|png)", parsed.query.lower()):
+        return False
+    if re.match(r"^(replytocom=|share=|action=(login|edit)|ical=).*", parsed.query.lower()):
+        return False
+    return True
     
     
