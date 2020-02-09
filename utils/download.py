04d51df9c5c117d160e6705179ebc797b6e7fb63
Alex-Mulligan <mulligaa@uci.edu>
2020-02-08 17:14:17 -0800

    Added timeout and redirect checks for download

--- utils/download.py
+++ utils/download.py
@@ -6,13 +6,19 @@ from utils.response import Response
 
 def download(url, config, logger=None):
     host, port = config.cache_server
-    resp = requests.get(
-        f"http://{host}:{port}/",
-        params=[("q", f"{url}"), ("u", f"{config.user_agent}")])
-    if resp:
-        return Response(cbor.loads(resp.content))
-    logger.error(f"Spacetime Response error {resp} with url {url}.")
-    return Response({
-        "error": f"Spacetime Response error {resp} with url {url}.",
-        "status": resp.status_code,
-        "url": url})
+    try:
+        resp = requests.get(
+            f"http://{host}:{port}/",
+            params=[("q", f"{url}"), ("u", f"{config.user_agent}")], timeout=5, allow_redirects=False) #added timeout, redirects, and try except blocks
+        if resp:
+            return Response(cbor.loads(resp.content))
+        logger.error(f"Spacetime Response error {resp} with url {url}.")
+        return Response({
+            "error": f"Spacetime Response error {resp} with url {url}.",
+            "status": resp.status_code,
+            "url": url})
+    except Exception as e:
+        return Response({
+            "error":"",
+            "status":999,
+            "url":url})
